867e1f95512c5e90ba8ba0db6a3d54b5c191bb90
Pius Agyekum <agyekumpius1@gmail.com>
2023-04-03 11:23:25 +0000

    Content base comment

--- src/Home.js
+++ src/Home.js
@@ -177,51 +177,6 @@ const Home = () => {
             </section>
 
 
-            <section className="section bg-grey" id="feature">
-                <div className="container">
-                    <div className="row justy-content-center">
-                        <div className="col-lg-3 col-sm-6 col-md-6">
-                            <div className="text-center feature-block">
-                                <div className="img-icon-block mb-4">
-                                    <i className="ti-thumb-up"></i>
-                                </div>
-                                <h4 className="mb-2">The right advice</h4>
-                                <p>Our team are experts in matching you with the right provider.</p>
-                            </div>
-                        </div>
-
-                        <div className="col-lg-3 col-sm-6 col-md-6">
-                            <div className="text-center feature-block">
-                                <div className="img-icon-block mb-4">
-                                    <i className="ti-cup"></i>
-                                </div>
-                                <h4 className="mb-2">Recognised for excellence</h4>
-                                <p>We've been awarded for our high rate of customer satisfaction.</p>
-                            </div>
-                        </div>
-
-                        <div className="col-lg-3 col-sm-6 col-md-6">
-                            <div className="text-center feature-block">
-                                <div className="img-icon-block mb-4">
-                                    <i className="ti-wallet"></i>
-                                </div>
-                                <h4 className="mb-2">Compare the best</h4>
-                                <p>We only compare market leaders with a reputation for service quality.</p>
-                            </div>
-                        </div>
-
-                        <div className="col-lg-3 col-sm-6 col-md-6">
-                            <div className="text-center feature-block">
-                                <div className="img-icon-block mb-4">
-                                    <i className="ti-dashboard"></i>
-                                </div>
-                                <h4 className="mb-2">Premium Services</h4>
-                                <p>We only compare market leaders with a reputation for service quality.</p>
-                            </div>
-                        </div>
-                    </div>
-                </div> {/* / .container */}
-            </section>
 
 
 
